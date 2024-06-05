507c8470323c4821bd2c071877486b642fd34c1b
Yehuhub <yhrcch@gmail.com>
2024-06-05 22:24:36 +0300

    added animation class

---
+++ include/Animation.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Utilities.h"
+
+
+//animation class is based on image of animation being in same row and seperated by 1 pixel
+//start position is the true top left of the the first image
+//image count is unsigned for how many images are in the animation
+class Animation{
+public:
+    Animation(sf::Vector2u startPosition, int imageCount, float switchTime, int spriteWidth = tileSize, int spriteHeight = tileSize);
+
+    void update(const sf::Time& deltaTime, bool flip = false);
+
+    //for when changing animation for an object
+    //i.e if link has a shield or not
+    void setStartPoisition(sf::Vector2u);
+
+    //returns the correct rect to use with the sprite
+    sf::IntRect getuvRect()const;
+
+private:
+    sf::Vector2u m_animationStartPosition;//(x,y) in pixels for start position for specific animation
+    int m_imageCount; //(x,y) x- how many rows of images, y- how many images in a row
+    int m_currentImage;
+
+    sf::IntRect m_uvRect;
+    bool m_singleImgFlip;
+
+    float m_timePassed;//time passed from last image change
+    float m_switchTime; //switch interval between each picture
+
+};
---
+++ src/Animation.cpp
@@ -0,0 +1,63 @@
+#include "Animation.h"
+#include <iostream>
+
+Animation::Animation(sf::Vector2u startPosition ,int imageCount, float switchTime, int spriteWidth, int spriteHeight)
+    : m_animationStartPosition(startPosition), m_imageCount(imageCount), m_currentImage(0),
+     m_timePassed(0), m_switchTime(switchTime), m_singleImgFlip(true){
+    
+    m_uvRect.top = startPosition.y;
+    m_uvRect.left = startPosition.x;
+
+    m_uvRect.width = spriteWidth;
+    m_uvRect.height = spriteHeight;
+}
+
+//flip is to mirror image according to x value
+void Animation::update(const sf::Time& deltaTime, bool flip) {
+    
+    m_timePassed += deltaTime.asSeconds();
+
+    //if only a single image is to be drawn it will be flipped with each time interval
+    if (m_imageCount == 1) {
+        if (m_timePassed >= m_switchTime) {
+            m_timePassed -= m_switchTime;
+
+            m_uvRect.width = -m_uvRect.width;
+            m_uvRect.left = m_animationStartPosition.x + abs(m_uvRect.width) * m_singleImgFlip;
+
+            m_singleImgFlip = ! m_singleImgFlip;
+        }
+    }
+    else{
+
+        if (m_timePassed >= m_switchTime) {
+            m_timePassed -= m_switchTime;
+            m_currentImage++;
+
+            if (m_currentImage >= m_imageCount) {
+                m_currentImage = 0;
+            }
+        }
+        if (!flip) {
+            m_uvRect.left = m_animationStartPosition.x + m_currentImage * m_uvRect.width + (1 * m_currentImage);
+            m_uvRect.width = abs(m_uvRect.width);
+        } 
+        else {
+            m_uvRect.left = m_animationStartPosition.x + ((m_currentImage + 1) * abs(m_uvRect.width)) + (1 * m_currentImage);
+            m_uvRect.width = -abs(m_uvRect.width);
+        }
+    }
+
+    m_uvRect.top = m_animationStartPosition.y;
+}
+
+void Animation::setStartPoisition(sf::Vector2u startPosition){
+    m_animationStartPosition = startPosition;
+    m_currentImage = 0;
+    m_uvRect.left = startPosition.x;
+    m_uvRect.top = startPosition.y;
+}
+
+sf::IntRect Animation::getuvRect()const{
+    return m_uvRect;
+}
