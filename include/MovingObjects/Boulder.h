856be3e13a8e1c686eb3f882e125a09fd46c3a11
orshain <orshain1@gmail.com>
2024-06-19 08:08:29 +0300

    added pigWarrior class, movment strategy design pattarn, edit enemys sprite sheet, added collitions of the pig and start of attak stage for the pig

--- include/MovingObjects/Boulder.h
+++ include/MovingObjects/Boulder.h
@@ -11,6 +11,11 @@ public:
 
 	virtual void update(const sf::Time&) override;
 	virtual void handleCollision() override;
+	virtual const sf::Vector2u& getAnimationTexturePosition(Input) override 
+	{
+		return ANIMATIONS_POSITIONS::PigWarriorRight;
+	};
+
 
 private:
 	static bool m_registerit;
